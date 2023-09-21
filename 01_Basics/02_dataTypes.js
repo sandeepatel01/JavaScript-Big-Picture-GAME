b9dc33b455bf49ec1619b9bf9190f07f320cecbc
Sandeep Patel <115567883+sandeepatel01@users.noreply.github.com>
2023-09-21 13:49:10 +0000

    ...

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -6,10 +6,10 @@ console.log(3
     +
      3) // code readability should be high
 
-console.log("Hitesh")
+console.log("Sandy")
 
 
-let name = "hitesh"
+let name = "Sandy"
 let age = 18
 let isLoggedIn = false
 let state;
