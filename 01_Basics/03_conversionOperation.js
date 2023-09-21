fffebc5127b39175493f1e5fc84709f46f7abcef
Sandeep Patel <115567883+sandeepatel01@users.noreply.github.com>
2023-09-21 15:41:52 +0000

    ...

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,46 @@
+// let score = "45";
+// let score = "45abc";
+// let score = null;
+// let score = undefined;
+// let score = true;
+let score = "sandy";
+
+
+
+
+
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+// let isLoggedIn = 1;
+// let isLoggedIn = " ";
+// let isLoggedIn = "";
+// let isLoggedIn = 0;
+let isLoggedIn = "Sandy";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//  1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let number = 55;
+
+console.log(number);
+console.log(typeof number);
+let stringNumber = String(number);
+console.log(stringNumber);
+console.log(typeof stringNumber);
