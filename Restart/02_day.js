596474184c5cb4e35a1eb52bde681fa8141ffa6d
Sandeep Patel <sandeepatel.0101@gmail.com>
2024-01-18 22:20:24 +0530

    datatypes in Javascript

---
+++ Restart/02_day.js
@@ -0,0 +1,33 @@
+// Variables in JavaScript 
+//  1. var 
+// 2. const 
+// 3. let  constly used 
+
+const productId = 1234;
+var userpASSWORD = '7880784204';
+let userEmail = "google@gmail.com";
+
+
+console.table([productId, userEmail, userpASSWORD]);
+
+
+
+// Datatypes in JavasScript 
+//    1. Primitive datatypes 
+//           Number
+//           String
+//           Boolean
+//           null 
+//           undefined 
+//           Bigint 
+
+//     2. Reference datatypes 
+//            Object
+//            Array
+//            Function      
+
+console.log(typeof "Sandy");
+console.log(typeof userpASSWORD);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof productId);
