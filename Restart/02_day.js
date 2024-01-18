efb8290a35c29e2d0cf2f80d9dfe5627bf731bbd
Sandeep Patel <sandeepatel.0101@gmail.com>
2024-01-18 23:04:23 +0530

    datatypes conversion in Javascript

--- Restart/02_day.js
+++ Restart/02_day.js
@@ -8,7 +8,7 @@ var userpASSWORD = '7880784204';
 let userEmail = "google@gmail.com";
 
 
-console.table([productId, userEmail, userpASSWORD]);
+// console.table([productId, userEmail, userpASSWORD]);
 
 
 
@@ -26,8 +26,46 @@ console.table([productId, userEmail, userpASSWORD]);
 //            Array
 //            Function      
 
-console.log(typeof "Sandy");
-console.log(typeof userpASSWORD);
-console.log(typeof null);
-console.log(typeof undefined);
-console.log(typeof productId);
+// console.log(typeof "Sandy");
+// console.log(typeof userpASSWORD);
+// console.log(typeof null);
+// console.log(typeof undefined);
+// console.log(typeof productId);
+
+
+// ************* Conversion Operation in JavaScript *************** 
+
+// let gameScore = 33;
+// let gameScore = "33";
+let gameScore = "33aaa";
+
+// console.log(typeof (gameScore));
+// console.log(typeof gameScore);
+
+let valueInNumber = Number(gameScore);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// let isLogout = 1;
+// let isLogout = 0;
+// let isLogout = "";
+// let isLogout = " ";
+// let isLogout = 2;
+// let isLogout = 3;
+let isLogout = "Sandy";
+
+let valueInBoolean = Boolean(isLogout);
+// console.log(typeof valueInBoolean);
+// console.log(valueInBoolean);
+
+
+let number = 51;
+
+console.log(number);
+console.log(typeof number);
+
+let valueInString = String(number);
+console.log(typeof valueInString);
+console.log(valueInString);
