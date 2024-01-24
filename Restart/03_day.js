c5396097b73a53c75481f18f4a00a2df03f1434d
Sandeep Patel <sandeepatel.0101@gmail.com>
2024-01-24 22:56:29 +0530

    String & String methods in JavaScript

--- Restart/03_day.js
+++ Restart/03_day.js
@@ -83,6 +83,76 @@ const myFunction = function () {
 // console.log(typeof anotherId);
 // console.log(typeof bigNumber);
 
-console.log(typeof heros);
-console.log(typeof userDetials);
-console.log(typeof myFunction);
+// console.log(typeof heros);
+// console.log(typeof userDetials);
+// console.log(typeof myFunction);
+
+
+
+
+// ********************************* String In JavaScript *************************
+
+const name = "Sandeep";
+const repoCount = 25;
+
+// console.log(name + repoCount);
+// console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
+
+// String Methods 
+const discordName = new String('Sandeep-pt');
+
+// console.log(discordName[0]);
+// console.log(discordName[6])
+// console.log(discordName.__proto__);
+
+
+console.log(discordName.length);
+console.log(discordName.toUpperCase());
+console.log(discordName.toLowerCase());
+
+console.log(discordName.charAt(1));
+console.log(discordName.charAt(0))
+console.log(discordName.charAt(6));
+
+console.log(discordName.indexOf('S'));
+console.log(discordName.indexOf('p'));
+console.log(discordName.indexOf('d'));
+
+
+// const newString = discordName.substring(0, 4);
+// const newString = discordName.substring(1, 4);
+// const newString = discordName.substring(3, 6);
+// const newString = discordName.substring(-3, 7);
+const newString = discordName.substring(3, 7);
+console.log(newString);
+
+
+// const anotherString = discordName.slice(-7, 3);
+// const anotherString = discordName.slice(-7, 7);
+const anotherString = discordName.slice(2, 7);
+console.log(anotherString)
+
+
+const newStringOne = "   Sandy       ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+
+//    replace & include[t/f] method 
+const url = "https://sandeep.com/sandeep%20patel"
+console.log(url.replace('%20', '_'));
+console.log(url.includes('Sandy'))
+
+let sentence = 'This is a sample sentence.';
+let includesSample = sentence.includes('sample'); // Returns true
+
+console.log(includesSample);
+
+
+// console.log(discordName.split('-'));
+console.log(discordName.split(' '));
+// console.log(discordName.split(' ', '-'));
+
+
