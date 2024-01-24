31109aa5006a551e6c838be589db9d0ac4d282d8
Sandeep Patel <sandeepatel.0101@gmail.com>
2024-01-24 17:15:11 +0530

    Comparision in JavaScript

--- Restart/03_day.js
+++ Restart/03_day.js
@@ -1,2 +1,27 @@
 console.log("Restart With JavaScript");
 
+// Comparision In JavaScript 
+
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(3 >= 3);
+// console.log(5 <= 4);
+// console.log(1 == 1);
+
+// console.log("2" == 2);
+// console.log("2" === 2);
+// console.log("03" > 2);
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+// console.log(null == 0);
+// console.log(null === 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+console.log(undefined === 0);
