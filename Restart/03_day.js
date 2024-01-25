281cde64555764a9918366b5c263e495182abf75
Sandeep Patel <sandeepatel.0101@gmail.com>
2024-01-26 00:01:45 +0530

    Arrow function

--- Restart/03_day.js
+++ Restart/03_day.js
@@ -845,7 +845,7 @@ console.log(addone(5))
 function addone(num) {    // only fxn declaretion
     return num + 1
 }
-console.log(addone(5))
+// console.log(addone(5))
 
 
 
@@ -856,4 +856,76 @@ const addTwo = function (num) {  //fxn declaration + variable hold(addTwo)
     return num + 2
 }
 addTwo(5)
-console.log(addTwo(9));
+// console.log(addTwo(9));
+
+
+
+
+
+// ************** Arrow Function IN JS **************
+
+const user1 = {
+    username: "sandeep",
+    price: 999,
+
+    welcomeMessage: function () {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user1.welcomeMessage()
+// user1.username = "sandy"
+// user1.welcomeMessage()
+
+// console.log(this);
+
+
+function chai1() {
+    console.log(this);
+}
+// chai1()
+
+
+function chai2() {
+    let username = "sandeep"
+    console.log(this.username);
+}
+// chai2()
+
+
+const chai3 = function () {
+    let username = "hitesh"
+    console.log(this.username);
+}
+// chai3()
+
+const chai4 = () => {
+    let username = "hitesh"
+    console.log(this.username);
+    console.log(this);
+}
+// chai4()
+
+
+
+const addTwo = (num1, num2) => {     // explicite return
+    return num1 + num2
+}
+// console.log(addTwo(2, 3));
+
+
+const addTwo1 = (num1, num2) => num1 + num2  // implicite return
+// console.log(addTwo1(1, 1));
+
+
+const addTwo2 = (num1, num2) => (num1 + num2)
+// console.log(addTwo2(9, 9));
+
+const addTwo3 = (num1, num2) => ({ username: "sandeep" })
+console.log(addTwo3(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+// myArray.forEach(() => {})
