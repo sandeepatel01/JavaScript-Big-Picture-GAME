9cc36b00c74c3e82bfe26c8a14c729fc56ad3f2f
Sandeep Patel <sandeepatel.0101@gmail.com>
2024-01-25 15:29:41 +0530

    Date && Time in JavaScript

--- Restart/03_day.js
+++ Restart/03_day.js
@@ -323,3 +323,77 @@ console.log(Math.floor(Math.random() * 10) + 1);
 const min = 10
 const max = 20
 console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+// toExponential(fractionDigits) method 
+let num = 123.456;
+console.log(num.toExponential(2));  // "1.23e+2"
+
+// valueOf() method 
+let numObj = new Number(42);
+let numPrimitive = numObj.valueOf();  // 42
+
+
+
+
+//***************/ Date And Time In JavaScript ******************
+
+// dates  in javaScript 
+
+let todate = new Date();
+
+// console.log(todate);
+// console.log(todate.getDate());
+// console.log(todate.getDay());
+// console.log(todate.getFullYear());
+// console.log(todate.getHours());
+// console.log(todate.getMilliseconds());
+// console.log(todate.getMinutes());
+// console.log(todate.getMonth());
+// console.log(todate.getMonth());
+// console.log(todate.getMonth() + 1);
+// console.log(todate.getSeconds());
+// console.log(todate.getTime());
+// console.log(todate.getTimezoneOffset());
+// console.log(todate.getUTCDate());
+// console.log(todate.getUTCDay());
+
+// console.log(todate.toString());
+// console.log(todate.setDate());
+// console.log(todate.Date());
+
+// console.log(todate.toDateString());
+// console.log(todate.toString());
+// console.log(todate.toLocaleString());
+
+
+// console.log(typeof todate);
+
+
+// let myCreateDate = new Date(2023, 7, 15)
+// let myCreateDate = new Date(2023, 7, 15, 5, 3)
+let myCreateDate = new Date("2023-01-15")
+
+// console.log(myCreateDate);
+// console.log(myCreateDate.toDateString());
+// console.log(myCreateDate.toLocaleString());
+
+
+
+let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+
+console.log(Date.now()); // milisecond 
+console.log(Math.floor(Date.now() / 1000));     // second
+
+let newDate = new Date();
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+
+})
+
