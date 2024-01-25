ecdaffa04c395eeedeb3c5f07413e92b3f8a42be
Sandeep Patel <sandeepatel.0101@gmail.com>
2024-01-25 19:29:07 +0530

    anOther Methods in JavaScript

--- Restart/03_day.js
+++ Restart/03_day.js
@@ -420,7 +420,7 @@ const myArr = new Array(10, 20, 30, 40, 50);
 // Array methods
 
 myArr.push(60)
-console.log(myArr);
+// console.log(myArr);
 
 // myArr.push(70)
 // console.log(myArr);
@@ -452,14 +452,50 @@ const newArr = myArr.join()
 
 // slice, splice
 
-console.log("A ", myArr);
+// console.log("A ", myArr);
 
 const myn1 = myArr.slice(1, 3)
 
-console.log(myn1);
-console.log("B ", myArr);
+// console.log(myn1);
+// console.log("B ", myArr);
 
 
 const myn2 = myArr.splice(1, 3)
-console.log(myn2);
-console.log("C ", myArr);
+// console.log(myn2);
+// console.log("C ", myArr);
+
+
+// ************************** Another Methods in Arrays ***********************************
+
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros]  // sprade operator
+// console.log(all_new_heros);
+
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+// const real_another_array = another_array.flat(Infinity)
+// const real_another_array = another_array.flat(1)
+const real_another_array = another_array.flat(2)
+// console.log(real_another_array);
+
+
+
+// console.log(Array.isArray("sandy"))
+// console.log(Array.from("sandy"))
+// console.log(Array.from({ name: "sandy" })) // interesting
+
+let score0 = 100
+let score2 = 200
+let score3 = 300
+
+// console.log(Array.of(score0, score2, score3));
